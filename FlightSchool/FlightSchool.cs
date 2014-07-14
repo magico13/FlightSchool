2f093a3370cb68373108102a7b72b07c99335af8
magico13 <magico1313@gmail.com>
2014-07-14 16:17:56 -0500

    Initial Commit

---
+++ FlightSchool/FlightSchool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using KSP;
+
+namespace FlightSchool
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    public class FlightSchool : MonoBehaviour
+    {
+        public void Awake()
+        {
+
+        }
+        
+        private void OnDraw()
+        {
+            
+        }
+
+        private void OnWindow(int windowID)
+        {
+            
+        }
+
+        public void Start()
+        {
+            Debug.Log("[FS] Success!");
+            
+        }
+
+        public void FixedUpdate()
+        {
+            
+        }
+
+    }
+}
